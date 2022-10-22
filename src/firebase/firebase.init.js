0adae47994616b8b574219ffd11e999ca31af671
abu bokor suraj <abusurajbd@gmail.com>
2022-10-22 11:27:47 +0600

    environment vairable added

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyClEpquE7GVaItw8z_Ml_qmqGnU3NsxZEQ",
-  authDomain: "barohal-news.firebaseapp.com",
-  projectId: "barohal-news",
-  storageBucket: "barohal-news.appspot.com",
-  messagingSenderId: "384736028781",
-  appId: "1:384736028781:web:bc06a24b465d4d09e5f764",
-  measurementId: "G-TCEJRC5CQW"
+  apiKey:  process.env.REACT_APP_API_KEY,
+  authDomain:  process.env.REACT_APP_API_AUTH_DOMAIN,
+  projectId:  process.env.REACT_APP_PROJECT_ID,
+  storageBucket:  process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:  process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:  process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
